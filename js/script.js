939b0f581aeb48342418ccb3b5f85c45ed65abb7
muhaguzan <mgustifauzan2794@gmail.com>
2024-03-05 20:48:02 +0700

    up

--- js/script.js
+++ js/script.js
@@ -10,7 +10,28 @@ window.onscroll = function() {
     }
 }
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw7QwzbZWA3e7xpDOL_Or95tGsL-uH38k-097QNVp2M3IfH3tx5-dWuWT62R7QRO4o8mg/exec'
+        const form = document.forms['contact-form']
+        const btnKirim = document.querySelector('.btn-kirim');
+        const btnLoading = document.querySelector('.btn-loading');
+        const myAlert = document.querySelector('.myalert');
+        
+      
+        form.addEventListener('submit', e => {
+          e.preventDefault()
+          btnLoading.classList.toggle('hidden');
+            btnKirim.classList.toggle('hidden');
 
+          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {
+                btnLoading.classList.toggle('hidden');
+                btnKirim.classList.toggle('hidden');
+                myAlert.classList.toggle('hidden')
+                console.log('Success!', response)
+                form.reset();
+        })
+            .catch(error => console.error('Error!', error.message))
+        })
 
 // hamburger
 const hamburger = document.querySelector('#hamburger');
